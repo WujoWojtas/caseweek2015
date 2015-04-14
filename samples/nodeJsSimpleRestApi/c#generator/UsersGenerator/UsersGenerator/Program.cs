a439f0838a55030a95f30330eefabe4077b5b4ac
WujoWojtas <wujowojtas@gmail.com>
2015-04-14 21:44:08 +0200

    simple c# request generator

---
+++ samples/nodeJsSimpleRestApi/c#generator/UsersGenerator/UsersGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using RestSharp;
+using System;
+using System.Threading;
+
+namespace UsersGenerator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            RestClient rc = new RestSharp.RestClient("http://localhost:1337/user");
+            int counter = 0;
+            while (true)
+            {
+                try
+                {
+                    RestRequest req = new RestRequest(Method.POST);
+                    var usr = new { firstName = "Jan" + counter, lastName = "Kowalsky" + counter };
+                    req.AddJsonBody(usr);
+
+                    string response = rc.Execute(req).Content;
+                    if (!string.IsNullOrWhiteSpace(response))
+                    {
+                        Console.WriteLine(response);
+                    }
+                    counter++;
+                }
+                catch { }
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ samples/nodeJsSimpleRestApi/c#generator/UsersGenerator/UsersGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UsersGenerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UsersGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("22e9641f-768b-4d8b-ba9d-1b909d1a9a25")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
