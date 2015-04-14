0c82fee083dbc5e157ef22968dd24c4638e90ce9
usr_wsipa <wojciech.sipa@comarch.pl>
2015-04-14 07:55:10 +0200

    files

---
+++ samples/nodeJsSimpleServer/server.js
@@ -0,0 +1,14 @@
+﻿function startServer() {
+	var http = require('http');
+	var counter = 0;
+	
+	http.createServer(function (req, res) {
+		res.writeHead(200, {
+			'Content-Type' : 'text/plain'
+		});
+		res.end('Hello World\n' + ++counter);
+		
+	}).listen(1337, '127.0.0.1');
+	console.log('Server running at http://127.0.0.1:1337/');
+}
+exports.startServer = startServer;
